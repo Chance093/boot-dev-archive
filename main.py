1b519119f99c516be9a8fd6f98bc652bca0b11e1
Chance093 <chance.dev093@gmail.com>
2024-05-20 23:19:40 -0700

    finish project

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    char_count = get_char_dict(text)
-    print(f"{num_words} words found in the document")
-    print(char_count)
-
+    char_dict = get_char_dict(text)
+    sorted_char_list = get_sorted_list(char_dict)
+    print_output(book_path, num_words, sorted_char_list)
 
 def get_num_words(text):
     words = text.split()
@@ -25,5 +24,24 @@ def get_char_dict(text):
         else:
             char_count[lowered_char] = 1
     return char_count
+
+def sort_on(dict):
+    return dict["num"]
+
+def get_sorted_list(dict):
+    dict_list = []
+    for char in dict:
+        if char.isalpha():
+            dict_list.append({"char": char, "num": dict[char]})
+    dict_list.sort(reverse=True, key=sort_on)
+    return dict_list
+
+def print_output(book_path, num_words, sorted_char_array):
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print("")
+    for dict in sorted_char_array:
+        print(f"The '{dict["char"]}' character was found {dict["num"]} times")
+    print("--- End report ---")
 
 main()
