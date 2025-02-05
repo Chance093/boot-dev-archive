6a98ccc990031bcbb0176670d3ba4ab011b82f0c
Chance093 <chance.dev093@gmail.com>
2025-02-04 16:18:01 -0800

    add addfeed command

--- handle_agg.go
+++ handle_agg.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"time"
+
+	"github.com/Chance093/gator/internal/database"
+	"github.com/google/uuid"
 )
 
 func handlerAgg(_ *state, _ command) error {
@@ -14,5 +19,35 @@ func handlerAgg(_ *state, _ command) error {
   }
 
   fmt.Println(rssFeed)
+  return nil
+}
+
+func handlerAddFeed(s *state, cmd command) error {
+  if len(cmd.args) != 2 {
+    return errors.New("provide name and url args in that order")
+  }
+
+  name := cmd.args[0]
+  url := cmd.args[1]
+  ctx := context.Background()
+
+  user, err := s.db.GetUser(ctx, s.cfg.Current_user_name)
+  if err != nil {
+    return fmt.Errorf("could not get user: %v", err)
+  }
+
+  feed, err := s.db.CreateFeed(ctx, database.CreateFeedParams{
+    ID: uuid.New(),
+    CreatedAt: time.Now().UTC(),
+    UpdatedAt: time.Now().UTC(),
+    Name: name,
+    Url: url,
+    UserID: user.ID,
+  })
+  if err != nil {
+    return fmt.Errorf("could not create feed: %v", err)
+  }
+
+  fmt.Printf("feed added: %v", feed)
   return nil
 }
