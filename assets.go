55e9cc9713687fd34cdb0e1f3535e3612d9ae0ca
Chance093 <chance.dev093@gmail.com>
2025-04-04 13:08:33 -0700

    feat: add video upload with aws s3

--- assets.go
+++ assets.go
@@ -1,12 +1,12 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
-
-	"github.com/google/uuid"
 )
 
 func (cfg apiConfig) ensureAssetsDir() error {
@@ -16,9 +16,14 @@ func (cfg apiConfig) ensureAssetsDir() error {
 	return nil
 }
 
-func getAssetPath(videoID uuid.UUID, mediaType string) string {
+func getAssetPath(mediaType string) string {
+  // get random string
+  key := make([]byte, 32)
+	rand.Read(key)
+  randString := base64.RawURLEncoding.EncodeToString(key)
+
 	ext := mediaTypeToExt(mediaType)
-	return fmt.Sprintf("%s%s", videoID, ext)
+	return fmt.Sprintf("%s%s", randString, ext)
 }
 
 func (cfg apiConfig) getAssetDiskPath(assetPath string) string {
